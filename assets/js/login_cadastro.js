8fe108a283d527af887939bb2b216b7da0b22251
raygrs <rayane.greis@sptech.school>
2024-05-05 01:16:54 -0300

    validação na tela de cadastro e ajustes na tela de login ainda sem validação

--- assets/js/login_cadastro.js
+++ assets/js/login_cadastro.js
@@ -1,7 +1,50 @@
-function entrar(){
-  alert(`Enviado`)
+// function validar (){
+//   const USER = document.querySelectorAll('user');
+//   const SENHA = document.querySelectorAll('password');
+//   let valid = true;
+
+//   if (USER.value == '' || SENHA.value == '') {
+//     alert('Preencha todos os campos')
+//     valid = false;
+//   }else if (SENHA.value.length <= 6 || USER.value.length <= 6) {
+//     alert('Os campos devem conter mais de 6 caracteres')
+//   }
+//   else{
+//     alert('Formulario enviado com sucesso');
+//   }
+
+//   return valid;
+
+// }
+
+function entrar() {
+  const LOGIN = document.querySelector('.login');
+  const CADASTRO = document.querySelector('.cadastro');
+  LOGIN.classList.remove('hide');
+  CADASTRO.classList.add('hide');
+  const USER = document.querySelector('user');
+  const SENHA = document.querySelector('password');
+  let valid = true;
+
+  if (USER.value == '' || SENHA.value == '') {
+    alert('Preencha todos os campos')
+    valid = false;
+  }else if (SENHA.value.length <= 6 || USER.value.length <= 6) {
+    alert('Os campos devem conter mais de 6 caracteres')
+    valid = false;
+  }
+  else{
+    alert('Formulario enviado com sucesso');
+    valid = true;
+  }
+
+  return valid;
 }
 
-function cadastrar(){
-  alert(`Usuário cadastrado com sucesso !`)
+function cadastrar() {
+  const LOGIN = document.querySelector('.login');
+  const CADASTRO = document.querySelector('.cadastro');
+  CADASTRO.classList.remove('hide');
+  LOGIN.classList.add('hide');
+  validar();
 }
